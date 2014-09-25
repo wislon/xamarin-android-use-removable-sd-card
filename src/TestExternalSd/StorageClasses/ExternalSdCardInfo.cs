b78dc7599dd7f2e13cb3e2036e5f101a6358d3de
wislon <wislon@gmail.com>
2014-09-25 13:32:30 +1000

    Add ExternalSdCardInfo class which can be used to get removable SD card information

---
+++ src/TestExternalSd/StorageClasses/ExternalSdCardInfo.cs
@@ -0,0 +1,75 @@
+namespace TestExternalSd.StorageClasses
+{
+  public static class ExternalSdCardInfo
+  {
+    private static string _path = null;
+    private static bool? _isWriteable;
+    private static FileSystemBlockInfo _fileSystemBlockInfo = null;
+
+    /// <summary>
+    /// Quick property you can check after initialising 
+    /// </summary>
+    public static bool ExternalSdCardExists
+    {
+      get { return !string.IsNullOrWhiteSpace(GetExternalSdCardPath()); }
+    }
+
+    /// <summary>
+    /// Returns the path to External SD card (if there is one),
+    /// otherwise empty string if there isn't
+    /// </summary>
+    public static string Path
+    {
+      get { return _path ?? GetExternalSdCardPath(); }
+    }
+
+    /// <summary>
+    /// Returns whether the external SD card is writeable. The first
+    /// call to this is an expensive one, because it actually tries to
+    /// write a test file to the external disk
+    /// </summary>
+    public static bool IsWriteable
+    {
+      get { return _isWriteable ?? IsExternalCardWriteable(); }
+    }
+
+
+    /// <summary>
+    /// The values in the <see cref="FileSystemBlockInfo"/> object may have
+    /// changed depending on what's going on in the file system, so it repopulates relatively
+    /// expensively every time you read this property
+    /// </summary>
+    public static FileSystemBlockInfo FileSystemBlockInfo
+    {
+      get { return GetFileSystemBlockInfo(); }
+    }
+
+    private static FileSystemBlockInfo GetFileSystemBlockInfo()
+    {
+      if (!string.IsNullOrWhiteSpace(GetExternalSdCardPath()))
+      {
+        _fileSystemBlockInfo = ExternalSdStorageHelper.GetFileSystemBlockInfo(_path);
+        return _fileSystemBlockInfo;
+      }
+      return null;
+    }
+
+
+    private static bool IsExternalCardWriteable()
+    {
+      if (string.IsNullOrWhiteSpace(GetExternalSdCardPath()))
+      {
+        return false;
+      }
+
+      _isWriteable = ExternalSdStorageHelper.IsWriteable(_path);
+      return _isWriteable.Value;
+    }
+
+    private static string GetExternalSdCardPath()
+    {
+      _path = ExternalSdStorageHelper.GetExternalSdCardPath();
+      return _path;
+    }
+  }
+}
