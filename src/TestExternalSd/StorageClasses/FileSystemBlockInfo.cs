02fca8555f44c87a92604df9f9c57a6768da5308
wislon <wislon@gmail.com>
2014-09-25 13:36:37 +1000

    Added a custom FileSystemBlockInfo object for holding some of the StatFS information

---
+++ src/TestExternalSd/StorageClasses/FileSystemBlockInfo.cs
@@ -0,0 +1,35 @@
+﻿namespace TestExternalSd.StorageClasses
+{
+  /// <summary>
+  /// File system block info for a given path, populated by
+  /// <see cref="ExternalSdStorageHelper.GetFileSystemBlockInfo"/> method.
+  /// Note that that method call is a relatively expensive one.
+  /// </summary>
+  public class FileSystemBlockInfo
+  {
+    /// <summary>
+    /// The path you asked to check file allocation blocks for
+    /// </summary>
+    public string Path { get; set; }
+
+    /// <summary>
+    /// The file system block size, in bytes, for the given path
+    /// </summary>
+    public double BlockSizeBytes { get; set; }
+
+    /// <summary>
+    /// Total size of the file system at the given path
+    /// </summary>
+    public double TotalSizeBytes { get; set; }
+
+    /// <summary>
+    /// Available size of the file system at the given path
+    /// </summary>
+    public double AvailableSizeBytes { get; set; }
+
+    /// <summary>
+    /// Total free size of the file system at the given path
+    /// </summary>
+    public double FreeSizeBytes { get; set; }
+  }
+}
